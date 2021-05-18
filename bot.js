bc438d7424ee824618f68c7665e78fbf41e5cd94
N8te <natonting@gmail.com>
2021-05-17 21:06:20 -0700

    Removed slash command support for now

--- bot.js
+++ bot.js
@@ -12,39 +12,41 @@ function getRndInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
 
-// The data for our command
-const commandData = {
-  name: 'echo',
-  description: 'Replies with your input!',
-  options: [{
-    name: 'input',
-    type: 'STRING',
-    description: 'The input which should be echoed back',
-    required: true,
-  }],
-};
+//Commands are not supported in discordjs as of current commit
 
-client.once('ready', () => {
-  // Creating a global command
-  // client.application.commands.create(commandData);
-
-  // Creating a guild-specific command
-  client.guilds.cache.get('844048247546183680').commands.create(commandData);
-});
-
-// Command Handling
-client.on('interaction', interaction => {
-  // If the interaction isn't a slash command, return
-  if (!interaction.isCommand()) return;
-
-  // Check if it is the correct command
-  if (interaction.commandName === 'echo') {
-    // Get the input of the user
-    const input = interaction.options[0].value;
-    // Reply to the command
-    interaction.reply(input);
-  }
-});
+// // The data for our command
+// const commandData = {
+//   name: 'echo',
+//   description: 'Replies with your input!',
+//   options: [{
+//     name: 'input',
+//     type: 'STRING',
+//     description: 'The input which should be echoed back',
+//     required: true,
+//   }],
+// };
+//
+// client.once('ready', () => {
+//   // Creating a global command
+//   // client.application.commands.create(commandData);
+//
+//   // Creating a guild-specific command
+//   client.guilds.cache.get('844048247546183680').commands.create(commandData);
+// });
+//
+// // Command Handling
+// client.on('interaction', interaction => {
+//   // If the interaction isn't a slash command, return
+//   if (!interaction.isCommand()) return;
+//
+//   // Check if it is the correct command
+//   if (interaction.commandName === 'echo') {
+//     // Get the input of the user
+//     const input = interaction.options[0].value;
+//     // Reply to the command
+//     interaction.reply(input);
+//   }
+// });
 
 client.on('message', message => {
 	if (message.content === prefix + 'roll') {
@@ -55,11 +57,5 @@ client.on('message', message => {
     message.channel.send(getRndInteger(1,6));
   }
 });
-
-// client.on('message', msg => {
-//   if (msg.content === 'ping') {
-//     msg.channel.send('pong');
-//   }
-// });
 
 client.login(token);
